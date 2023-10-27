207534b1e1cbe94e8adace3db84c6b305430a4b3
thegouge <thegouge42@gmail.com>
2023-10-26 19:37:46 -0700

    wat

--- init.lua
+++ init.lua
@@ -22,7 +22,6 @@ vim.opt.rtp:prepend(lazypath)
 
 -- NOTE: Here is where you install your plugins.
 --  You can configure plugins using the `config` key.
---
 --  You can also configure plugins after the setup call,
 --    as they will be available in your neovim runtime.
 require('lazy').setup({
