7466589fb76dc346a8940304b0b1c298aca39ed7
thegouge <thegougedev@gmail.com>
2024-06-10 20:39:06 -0700

    Moving all of my keymaps and options to seperate files

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -123,6 +123,9 @@ local remaps = {
 
     -- Floaterm
     ['<A-i>'] = { ':FloatermToggle<CR>', 'Toggle Floating Terminal' },
+
+    -- Set highlight on search, but clear on pressing <Esc> in normal mode
+    ['<Esc>'] = { '<cmd>nohlsearch<CR>', 'clear Search' },
   },
   t = {
     ['<Esc>'] = { '<C-\\><C-n> :FloatermToggle<CR>', 'Exit Terminal Mode' },
@@ -145,6 +148,20 @@ require('which-key').register {
   ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
   ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
 }
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- TIP: Disable arrow keys in normal mode
+-- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- programmatically setting all of the above keymaps
 for mode, mappings in pairs(remaps) do
