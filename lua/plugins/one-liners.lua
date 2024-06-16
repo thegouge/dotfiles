88f8e7e11701df6eac16a741fda166ca6b507483
thegouge <thegougedev@gmail.com>
2024-06-15 19:11:20 -0700

    Integrating git!

--- lua/plugins/one-liners.lua
+++ lua/plugins/one-liners.lua
@@ -1,3 +1,10 @@
 return {
   { 'tpope/vim-sleuth' }, -- Detect tabstop and shiftwidth automatically
+  {
+    'FabijanZulj/blame.nvim',
+    config = function()
+      vim.keymap.set('n', '<leader>gB', ':BlameToggle virtual<CR>', { silent = true, desc = 'toggle [G]it [B]lame' })
+      require('blame').setup()
+    end,
+  },
 }
