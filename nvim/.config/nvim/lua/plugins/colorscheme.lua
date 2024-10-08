591fd5db64f027cdfd2a27c2129e20dab51f0bef
Alex Gouge-Schajer <thegougedev@gmail.com>
2024-10-07 21:56:40 -0700

    Allowing for dynamic color scheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,26 @@
 return {
-  -- {
-  --   'catppuccin/nvim',
-  --   name = 'catppuccin',
-  --   priority = 1000,
-  --   config = function()
-  --     vim.cmd.colorscheme 'catppuccin-mocha'
-  --   end,
-  -- },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+  },
   { -- You can easily change to a different colorscheme.
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.api.nvim_create_autocmd('BufEnter', {
+      --   desc = 'Change the colorscheme based on the file type',
+      --   callback = function()
+      --     local filetype = vim.bo.filetype
+      --     if filetype == 'go' then
+      --       vim.cmd.colorscheme 'catppuccin-mocha'
+      --     else
+      --       vim.cmd.colorscheme 'tokyonight-night'
+      --     end
+      --   end,
+      -- })
 
+      vim.cmd.colorscheme 'tokyonight-night'
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
