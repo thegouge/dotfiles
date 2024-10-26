b451d754d88dfad8ce100de201ef73f81c8604a5
Alex Gouge-Schajer <thegougedev@gmail.com>
2024-10-25 17:02:56 -0700

    adding my neovim?

---
+++ private_dot_config/nvim/README.md
@@ -0,0 +1,8 @@
+# TheGouge.nvim
+
+## Introduction
+
+This is my personal, curated configuration of neovim
+
+this configuration is HEAVILY inspired by [kickstart.nvim](https://github.com/nvim-lua/kickstart.nvim) with a bit of [typecraft's neovim for noobs](https://www.youtube.com/watch?v=zHTeCSVAFNY&list=PLsz00TDipIffreIaUNk64KxTIkQaGguqn&index=1)
+
---
+++ private_dot_config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,28 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '\\', ':Neotree toggle<CR>', { desc = 'Toggle Neotree' } },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+  config = function()
+    require('neo-tree').setup { close_if_last_window = true }
+  end,
+}
