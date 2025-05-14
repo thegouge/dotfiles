dcc505814fd3c43b824da825f0f3bb91b2c88bcb
Alex Gouge-Schajer <thegougedev@gmail.com>
2025-05-14 11:55:09 -0700

    Fixed install-packages.sh

--- bash/bash-scripts/install-packages.sh
+++ bash/bash-scripts/install-packages.sh
@@ -1,68 +1,49 @@
 #!/bin/bash
 
-
 # List of base packages to install, feel free to comment out what you don't want
 packages=(
-    "neovim" 
-    "tmux" 
-    "discord" 
-    "Godot" 
-    "gnome-terminal" 
-    "steam" 
-    "syncthing" 
-    "obs-studio" 
-    "zoom" 
-    "timeshift" 
-    "fzf" 
-    "stow",
-    "xclip")
+    "discord"
+    "docker"
+    "dolphin"
+    "fzf"
+    "ghostty"
+    "go"
+    "godot"
+    "gparted"
+    "hypridle"
+    "hyprlock"
+    "hyprshot"
+    "neovim"
+    "nerd-fonts-git"
+    "nodejs"
+    "npm"
+    "obs-studio"
+    "parted"
+    "steam"
+    "stow"
+    "syncthing"
+    "timeshift"
+    "tmux"
+    "vivaldi"
+    "wofi"
+    "wpaperd"
+    "xclip"
+    "yazi"
+    "zen-browser"
+    "zoom")
 
-# Double check that we're running as sudo
-if [ "$EUID" -ne 0 ];then
-    echo "Please run this script as root"
-    exit 1
-fi
-
-    # Adding all the repositories we need
-    add-apt-repository ppa:neovim-ppa/stable -y
-    add-apt-repository ppa:obsproject/obs-studio -y
-
-    apt-get update
-
-    # install everything
-    apt-get install "${packages[@]}" nala ffmpeg -y
-
-    # set up docker
-    apt-get install ca-certificates curl
-    install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    chmod a+r /etc/apt/keyrings/docker.asc
-    echo \ "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \ $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" | \ tee /etc/apt/sources.list.d/docker.list > /dev/null
-    apt-get update
-    apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-elif [ -f /etc/pacman.conf ]; then
-    echo "we're in Arch land!"
-
-    yay -S "${packages[@]}" docker go nerd-fonts-git -y
-
-else
-    echo "Unsupported distro!"
-fi
+sudo pacman -S yay
+yay -S "${packages[@]}"
 
 # Set up Node Version Manager
 # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 # nvm install node
 
 # Install vencord
-# sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"
+bash -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"
 
 # Append to our .bashrc
-echo "# set a fancy prompt (non-color, unless we know we "want" color)
-case "$TERM" in
-    xterm-color|*-256color) color_prompt=yes;;
-esac
-
+echo "
 # Alias Definitions
 # this line shoves all the aliases into a .bash_aliases file
 if [ -f ~/.bash_aliases ]; then
