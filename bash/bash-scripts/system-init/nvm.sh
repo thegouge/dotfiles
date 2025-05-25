e20d138b901ae80ca7c734f73621b9bac5402a2c
thegouge <thegougedev@gmail.com>
2025-05-25 13:59:14 -0700

    fixing the nvm installer

--- bash/bash-scripts/system-init/nvm.sh
+++ bash/bash-scripts/system-init/nvm.sh
@@ -2,4 +2,6 @@
 
 # Set up Node Version Manager
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install node
