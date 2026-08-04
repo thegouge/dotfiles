25e8ff10316c1149533e3c67e067a7586ecbb408
Alex Gouge-Schajer <thegougedev@gmail.com>
2026-08-04 07:49:23 -0700

    Update mount-storage.sh

--- bash/bash-scripts/system-init/mount-storage.sh
+++ bash/bash-scripts/system-init/mount-storage.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sudo echo "\n\n# 4TB Storage Drive\nUUID=4b95a440-41f3-40a2-b42c-da42632cbdaf /storage       btrfs   rw,noatime,exec 0 0" >> /etc/fstab
+sudo echo "
+
+# 4TB Storage Drive
+UUID=4b95a440-41f3-40a2-b42c-da42632cbdaf /storage       btrfs   rw,noatime,exec 0 0" >> /etc/fstab
