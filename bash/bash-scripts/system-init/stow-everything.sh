f18fd7cae701bcb9be8cf1d5193db0c6472b6fa4
Alex Gouge-Schajer <thegougedev@gmail.com>
2026-08-10 14:27:07 -0700

    adding actual functionality to "stow everything"

--- bash/bash-scripts/system-init/stow-everything.sh
+++ bash/bash-scripts/system-init/stow-everything.sh
@@ -1,21 +1,45 @@
 #!/bin/bash
 
-echo "Would you like to run everything? [y/N]"
-read theworks
+cd ~/dotfiles
 
-if [[ "$theworks" == "y" ]]; then
-
-  exit 0
+echo "Are you using Fish? [y/N]"
+read fish
+if [[ "$fish" == "y" ]]; then
+  stow fish
+else
+  stow bash
 fi
 
-fish
-ghostty
-hyprland
-nvim
-starship
-tmux
-wallpaper
-waybar
-wlogout
-wofi
-yazi
+echo "Are you on Hyprland? [y/N]"
+read hypr
+if [[ "$hypr" == "y" ]]; then
+  stow hyprland
+fi
+
+echo "are you using Ghostty? [Y/n]"
+read ghost
+if [[ "$ghost" != "n" ]]; then
+  stow ghostty
+fi
+
+echo "are you still a based developer? [Y/n]"
+read based
+if [[ "$based" != "n" ]]; then
+  stow nvim tmux
+fi
+
+echo "would you like to use Starship? [y/N]"
+read ship
+
+if [[ "$ship" == "y" ]]; then
+  stow starship
+fi
+
+echo "Would you like to use Yazi? [y/N]"
+read yazi
+
+if [[ "$yazi" == "y" ]]; then
+  stow yazi
+fi
+
+cd ~
